753ba32a60c6dd1d75564744abdfb4aebf49c69f
cebollada <46401489+cebollada@users.noreply.github.com>
2025-07-06 18:47:24 +0200

    Update servidor-app.py

--- servidor-app.py
+++ servidor-app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from datetime import datetime
-
+import os
 app = Flask(__name__)
 
 # Ruta para recibir datos del ESP32
@@ -24,4 +24,5 @@ def home():
     return "Servidor REGENBOX activo"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
